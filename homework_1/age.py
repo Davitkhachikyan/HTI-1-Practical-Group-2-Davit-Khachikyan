6a8837f9fc092f83900948e0ebac9cd79c40945f
Davit Khachikyan <davitkhachikyan93@gmail.com>
2020-11-30 02:37:37 +0400

    changed age.py

--- homework_1/age.py
+++ homework_1/age.py
@@ -1,18 +1,6 @@
-
-
-
-
-
-
-
-
-
-
-
-
-# year = input("Enter year: ")
-# if year[-2:] == "00":
-#     age = int(year) // 100
-# else:
-#     age = int(year) // 100 + 1
-# print(age)
+year = input("Enter year: ")
+if year[-2:] == "00":
+    age = int(year) // 100
+else:
+    age = int(year) // 100 + 1
+print(age)
