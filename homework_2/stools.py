e9904a1b60e7ea731d30004571a5e62bdd30df4d
Davit Khachikyan <davitkhachikyan93@gmail.com>
2020-12-02 22:27:00 +0400

    homework 2 stools

---
+++ homework_2/stools.py
@@ -0,0 +1,14 @@
+def stools(height):
+    int_list = []
+    summery = 0
+    for i in height:
+        int_list.append(int(i))
+    max_height = max(int_list)
+    for i in int_list:
+        x = max_height - i
+        summery += x
+    return summery
+
+
+user_input = input("Enter heights: ").split()
+print(stools(user_input))
